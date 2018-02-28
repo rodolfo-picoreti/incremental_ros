336efc51120291d4964be287c0811f6710f6630c
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2018-02-28 10:23:01 +0000

    + simple publisher with timer

--- src/hello.cpp
+++ src/hello.cpp
@@ -5,7 +5,20 @@
 char const* node_name = "hello_node";
 
 int main(int argc, char** argv) {
-  hello::Greeting greet;
-  greet.text = "Hello !!";
-  ROS_INFO("%s", greet.text.c_str());
+  ros::init(argc, argv, node_name);
+  ros::NodeHandle node;
+  auto greetPublisher = node.advertise<hello::Greeting>(/*topic=*/"greet", /*queue_size=*/1);
+
+  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
+
+  auto timer = node.createTimer(ros::Duration(/*seconds=*/0.1), [&](ros::TimerEvent const& ev) {
+    ros::Duration jitter = ev.current_real - ev.current_expected;
+    ROS_DEBUG("Publish jitter %d ms", static_cast<int>(jitter.sec * 1e3 + jitter.nsec / 1e6));
+    hello::Greeting greet;
+    greet.text = "Hello !!";
+    greetPublisher.publish(greet);
+  });
+
+  ROS_INFO("Running ...");
+  ros::spin();
 }
