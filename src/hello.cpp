b224a5c45b99fde74f0fc93d2ada753e62e78a53
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2018-03-04 18:17:55 +0000

    + basic ros launch file

--- src/hello.cpp
+++ src/hello.cpp
@@ -9,8 +9,10 @@ char const* node_name = "hello_node";
 int main(int argc, char** argv) {
   ros::init(argc, argv, node_name);
   ros::NodeHandle node;
-  auto greetPublisher = node.advertise<hello::Greeting>(/*topic=*/"greet", /*queue_size=*/1);
+
   // ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
+
+  auto greetPublisher = node.advertise<hello::Greeting>(/*topic=*/"greet", /*queue_size=*/1);
 
   dynamic_reconfigure::Server<hello::HelloConfig> server;
   hello::HelloConfig config;
@@ -20,6 +22,7 @@ int main(int argc, char** argv) {
     ros::Duration jitter = ev.current_real - ev.current_expected;
     ROS_DEBUG("Publish jitter %d ms", static_cast<int>(jitter.sec * 1e3 + jitter.nsec / 1e6));
     hello::Greeting greet;
+    greet.header.stamp = ros::Time::now();
     greet.text = "Hello " + config.greeter_name;
     greetPublisher.publish(greet);
   });
--- src/listen.cpp
+++ src/listen.cpp
@@ -6,17 +6,27 @@ char const* node_name = "listen_node";
 
 class Listener {
   ros::Subscriber greetSubscriber;
+  int greetSeq = -1;
 
  public:
   Listener(ros::NodeHandle& node) {
     // prefer udp over tcp
     auto hints = ros::TransportHints().unreliable().reliable().tcpNoDelay();
     greetSubscriber = node.subscribe(
-        /*topic=*/"greet", /*queue_size=*/1, &Listener::onGreet, this);
+        /*topic=*/"greet", /*queue_size=*/1, &Listener::onGreet, this, hints);
   }
 
-  void onGreet(hello::Greeting::ConstPtr const& msg) const {
-    ROS_INFO("Received greeting: %d %s", msg->header.seq, msg->text.c_str());
+  void onGreet(hello::Greeting::ConstPtr const& msg) {
+    auto latency = ros::Time::now() - msg->header.stamp;
+    if (greetSubscriber.getNumPublishers() == 1) {
+      // Sequence checking only works if we have a single publisher
+      auto lostPackets = msg->header.seq - greetSeq - 1;
+      if (greetSeq != -1 && lostPackets) { ROS_WARN("Lost %d message(s)", lostPackets); }
+      greetSeq = msg->header.seq;
+    }
+
+    ROS_INFO("[%d][%dus] Greeting:='%s'", msg->header.seq,
+             static_cast<int>(latency.sec * 1e6 + latency.nsec / 1e3), msg->text.c_str());
   };
 };
 
